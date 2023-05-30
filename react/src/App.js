5621b31b15109655583640d42fed0353f3702dc6
kabradshaw1 <itllbok82@gmail.com>
2023-05-30 13:45:24 -0400

    building demo containers

--- react/src/App.js
+++ react/src/App.js
@@ -4,55 +4,42 @@ import axios from 'axios';
 import useSWR from 'swr';
 
 function App() {
-	const baseURL = 'http://127.0.0.1/api/';
+	const baseURL = 'http://127.0.0.1:8000/api/';
 
 	const axiosInstance = axios.create({
 		baseURL: baseURL,
 		timeout: 5000,
 		header: {
 			'Context-Type': 'applicaiton/json',
-		};
+		}
 	});
 
 	function fetcher(url) {
 		return axiosInstance.get(url).then((res) => res.data);
 	};
 
-	const { data: posts, error, isLoading } = useSWR('', fetcher);
+	const { data: posts, error, isLoading } = useSWR('/', fetcher);
+	console.log(posts)
+	if(error) return <h1>Failed to load</h1>;
+	if(isLoading) return <h1>Loading...</h1>;
 
-	if(error) return <h1>failed to load</h1>
-	if(isLoading) return <h1>Loading...</h1>
+	const Post = ({id, title}) => {
+		return (
+			<p>{id} {title}</p>
+		)
+	};
 
   return (
-    <div>
-			Success
+		<div>
+			<h1>Success</h1>
 			{posts && posts.map((post) => {
-				<p>{post.id}</p>
+				return (
+					<Post key={post.id} id={post.id} title={post.title}/>
+				)
 			})}
-    </div>
+		</div>
   )
-	// const [appState, setAppState] = useState({
-	// 	loading: false,
-	// 	posts: null,
-	// });
 
-
-	// useEffect(() => {
-	// 	setAppState({ loading: true });
-	// 	const apiUrl = `http://127.0.0.1/api/`;
-	// 	fetch(apiUrl)
-	// 		.then((data) => data.json())
-	// 		.then((posts) => {
-	// 			setAppState({ loading: false, posts: posts });
-  //       console.log(appState)
-	// 		});
-	// }, [setAppState]);
-
-	// return (
-	// 	<div className="App">
-  //     <h1>react app check logs</h1>
-	// 	</div>
-	// );
 }
 
 export default App;
