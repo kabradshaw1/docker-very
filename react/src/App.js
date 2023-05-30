2ee7f1ef8b3c11a0d3680b4f00d0a1106f98369c
kabradshaw1 <itllbok82@gmail.com>
2023-05-30 11:31:30 -0400

    building demo react app

--- react/src/App.js
+++ react/src/App.js
@@ -1,33 +1,34 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
-
-
+import axios from 'axios';
+import useSWR from 'swr';
 
 function App() {
-	const [appState, setAppState] = useState({
-		loading: false,
-		posts: null,
+	const baseURL = 'http://127.0.0.1/api/';
+
+	const axiosInstance = axios.create({
+		baseURL: baseURL,
+		timeout: 5000,
+		header: {
+			'Context-Type': 'applicaiton/json',
+		};
 	});
 
+	function fetcher(url) {
+		return axiosInstance.get(url).then((res) => res.data);
+	};
 
-	useEffect(() => {
-		setAppState({ loading: true });
-		const apiUrl = `http://127.0.0.1/api/`;
-		fetch(apiUrl)
-			.then((data) => data.json())
-			.then((posts) => {
-				setAppState({ loading: false, posts: posts });
-        console.log(posts)
-			});
-	}, [setAppState]);
+	const { data: posts, error, isLoading } = useSWR('', fetcher);
 
+	if(error) return <h1>failed to load</h1>
+	if(isLoading) return <h1>Loading...</h1>
 
   return (
     <div>
-      <h1>done</h1>
-      {appState.posts && appState.posts.map((post) => {
-        return (<p>{post.id}</p>)
-      })}
+			Success
+			{posts && posts.map((post) => {
+				<p>{post.id}</p>
+			})}
     </div>
   )
 	// const [appState, setAppState] = useState({
